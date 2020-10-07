02d81773d2e9da397b8356c8bf4fa74598dd3fb7
Sergio Beltran <sergiobeltrang91@gmail.com>
2020-10-06 22:50:22 -0300

    working in first function

--- TP1.py
+++ TP1.py
@@ -14,16 +14,25 @@ datosParticipante = {
 
 def cargaParticipantes():
 
+    auxDisparoX = 0
+    auxDisparoY = 0
+
     while True:
 
-        datosParticipante['numeroId'] = int(input("Ingrese el número del participante: "))
+        datosParticipante['numeroId'] = int(input("Ingrese el número del participante (999 para finalizar): "))
         if datosParticipante['numeroId'] == 999:
+            print("Finalizó la carga de participantes")
             break
         else:
-            datosParticipante['nombreApellido'] = input("Ingrese el nombre y apellido del participante: ")
+            datosParticipante['nombreApellido'] = input("Ingrese el nombre y apellido del participante: ").lower()
             datosParticipante['edad'] = int(input("Ingrese la edad del participante: "))
-            datosParticipante['sexo'] = input("Ingrese el sexo del participante: ")
-            for disparoX in range(3):
-                datosParticipante['ubicacionDisparo'].append(int(input(f"Ingrese la coordenada del disparo en X (disparo{disparoX+1}): ")))
-                for disparoY in range(3):
-                    datosParticipante['ubicacionDisparo'].append(int(input(f"Ingrese la coordenada del disparo en Y (disparo{disparoY+1}): ")))
+            datosParticipante['sexo'] = input("Ingrese el sexo del participante: ").upper()
+            for disparo in range(3):
+                auxDisparoX = float(input(f"Ingrese la coordenada del disparo {disparo+1} en X: "))
+                auxDisparoY = float(input(f"Ingrese la coordenada del disparo {disparo+1} en Y: "))
+                datosParticipante['ubicacionDisparo'].append(round(distanciaAlOrigen(auxDisparoX, auxDisparoY),2))
+                
+    print(datosParticipante)
+    return datosParticipante
+
+prueba = cargaParticipantes()
